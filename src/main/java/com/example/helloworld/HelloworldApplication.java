a5b2ec30ce06ac7babbe3bef89970a0e04556ee4
ThomasWest3616 <70579609+ThomasWest3616@users.noreply.github.com>
2021-06-13 15:23:26 +0200

    Update HelloworldApplication.java

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -10,6 +10,9 @@ public class HelloworldApplication {
     public static void main(String[] args) {
 //        System.out.println(System.getProperty("java.version"));
         SpringApplication.run(HelloworldApplication.class, args);
+        
+     System.out.println("Hej med dig");    
+     
     }
 
 }
